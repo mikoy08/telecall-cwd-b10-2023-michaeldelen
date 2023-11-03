016e3055f5431199ade47cb1a37a3fecc396bf69
mikoy08 <mykel831@gmail.com>
2023-11-04 00:52:41 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+
+  // tabs
+    $('.tabs').tabs();
+
+  // sidenvav
+    $('.sidenav').sidenav({
+      inDuration: 500
+    });
+    
+  // dropdown
+    $('.dropdown-trigger').dropdown({
+      coverTrigger: false,
+    });
+
+  //  FAB
+    $('.fixed-action-btn').floatingActionButton({
+      hoverEnabled: false
+    });
+    
+  // material box
+    
+    
+
+
+  });
+
+  
